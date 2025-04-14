569fec0d3077e9fcbbb7947f821d8753ff4c2501
Yashraj692 <yashrajsangle692@gmail.com>
2025-04-14 20:40:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,17 @@ import matplotlib.pyplot as plt
 # need to give encoding because old dataset (Uses UK pound)
 df = pd.read_csv("data.csv", encoding='ISO-8859-1')
 
-# Drop rows with missing customer IDs and filter out returns
 df.dropna(subset=['CustomerID'], inplace=True)
 df = df[df['Quantity'] > 0]
 
-# Create a Revenue column
 df['Revenue'] = df['Quantity'] * df['UnitPrice']
 
-# Convert InvoiceDate to datetime
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
 
-# Set snapshot date (one day after the last invoice) this shows how long it's been since each customer purchased
+#this shows how long it's been since each customer purchased
 snapshot_date = df['InvoiceDate'].max() + dt.timedelta(days=1)
 
-# Build RFM table
-# recency = subtract last snapshot date and recent purchase date to calculate how long it has been
-# Count how many unique invoices/orders (InvoiceNo) the customer has.
-# More invoices = more purchases = higher frequency.
-# Monetary = Quantity * price
-
+# RFM table
 rfm = df.groupby('CustomerID').agg({
     'InvoiceDate': lambda x: (snapshot_date - x.max()).days,
     'InvoiceNo': 'nunique',
@@ -38,18 +30,15 @@ rfm = df.groupby('CustomerID').agg({
     'Revenue': 'Monetary'
 })
 
-# Normalize the RFM values
 scaler = StandardScaler()
 rfm_scaled = scaler.fit_transform(rfm)
-
 # Apply K-Means Clustering
 kmeans = KMeans(n_clusters=4, random_state=42)
 rfm['Cluster'] = kmeans.fit_predict(rfm_scaled)
 
 # Analyze the clusters
 print(rfm.groupby('Cluster').mean())
-
-# Optional: Visualize clusters
+# Visualization
 sns.scatterplot(data=rfm, x='Recency', y='Monetary', hue='Cluster', palette='Set2')
 plt.title("Customer Segments by Recency and Monetary Value")
 plt.show()
